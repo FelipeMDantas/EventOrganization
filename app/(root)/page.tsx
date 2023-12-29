444024c9fea0eb79a7b7169957e008fb924b0a2d
FelipeMDantas <felipemdantas@terra.com.br>
2023-12-28 21:37:26 -0300

    Clerk authentication integrated to the application.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,5 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <main>
-      <h1 className="text-4xl">Evently</h1>
-      <Button>Hello</Button>
-    </main>
-  );
+  return <main></main>;
 }
